0f8944c5bef521ea7c2083125b472bcf0b2b7e17
Patipan Roungsuawn <56648202+patipanice@users.noreply.github.com>
2021-05-17 10:23:28 +0700

    Update Rowmatch.js

--- Frontend/src/component/History/Rowmatch.js
+++ Frontend/src/component/History/Rowmatch.js
@@ -3,7 +3,16 @@ import UpTime from "./UpTime";
 import UpDate from "./UpDate";
 import "./Rowmatch.css";
 import ShowMatch from "./ShowMatch";
-import Table from "./Table";
-
+export default function Rowmatch(props) {
+  const {status} = props
+  return (
+ <tr>
+   <td>{status._id}</td>
+   <td><UpDate inputDate={status.start_date_time}/></td>
+   <td><StartTime inputStartTime={status.start_date_time}/></td>
+   <td><EndTime inputEndTime={status.end_date_time}/></td>
+   <td><Status gameRound={status.game_round}/></td>
+ </tr>
+  )
 
 }
