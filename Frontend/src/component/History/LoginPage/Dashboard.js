c90b675772b6d229a0382eed6676d315f3c8ec38
Patipan Roungsuawn <56648202+patipanice@users.noreply.github.com>
2021-05-18 22:10:38 +0700

    FixBug login, server side

--- Frontend/src/component/History/LoginPage/Dashboard.js
+++ Frontend/src/component/History/LoginPage/Dashboard.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import useToken from "./useToken";
 import LoginAdmin from "./LoginAdmin";
-import axios from "axios";
 import DelMatch from "./DelMatch";
 
 export default function Dashboard() {
@@ -11,17 +10,12 @@ export default function Dashboard() {
   if (!token) {
     return <LoginAdmin setToken={setToken} />;
   }
-  // useEffect(() => {
-  //   axios.get("http://localhost:5000/status").then((result) => {
-  //     const { data } = result;
-  //     //console.log(data);
-  //     setStatus(data);
-  //   });
-  // }, []);
-
   return (
     <>
       {" "}
+      <button onClick={()=> {
+        localStorage.clear();
+        }}>x</button>
       <h2>Admin</h2>
       <table className="styled-table">
         <thead>
--- Frontend/src/component/History/LoginPage/useToken.js
+++ Frontend/src/component/History/LoginPage/useToken.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
 export default function useToken() {
+  //const [token, setToken] = useState();
+  
   const getToken = () => {
-    const tokenString = localStorage.getItem("token");
+    const tokenString = localStorage.getItem('token');
     const userToken = JSON.parse(tokenString);
     return userToken?.token;
   };
 
-  const [token, setToken] = useState(getToken());
+ const [token, setToken] = useState(getToken());
+
 
   const saveToken = (userToken) => {
-    localStorage.setItem("token", JSON.stringify(userToken));
-    setToken(userToken.token);
+     localStorage.setItem("token", userToken);
+     setToken(userToken);
+     console.log(userToken);
   };
 
   return {
